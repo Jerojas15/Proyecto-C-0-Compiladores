5449e5fdc7218c8f170d1e4efd74983f453a9946
ScarletJenkinsFernandez <sca_ajf@hotmail.com>
2017-06-10 15:08:40 -0600

    Add files via upload

--- ProyectoC0Compiladores/src/structures/FinalCode.java
+++ ProyectoC0Compiladores/src/structures/FinalCode.java
@@ -35,12 +35,9 @@ public class FinalCode {
     private void processLine(IntermediateLine line){
         String space = "        ";
         switch(line.command){
-            case "END":
-                this.file.write("END");
-                break;
             case "PUT_CONSTANT":
-                String temp = line.op1+": DATA"+space;
-                this.file.write(temp.substring(0,space.length())+line.res);
+                String temp = line.op1+": DATA ";
+                this.file.write(temp+line.res);
                 break;
             case "PRINT_CONSTANT":
                 this.file.write(space+"WRSTR /"+line.op1);
@@ -49,14 +46,15 @@ public class FinalCode {
                 this.file.write(space+"WRINT /"+line.op1);
                 break;
             case "LABEL":
-                String temp2 = line.res+":"+space;
-                this.file.write(temp2.substring(0,space.length())+"NOP");
+                String temp2 = line.res+": ";
+                this.file.write(temp2+"NOP");
                 break;
             case "JUMP_LABEL":
                 this.file.write(space+"BR /"+line.res);
                 break;
             case "JUMP_CONDITION":
                 this.file.write(space+"CMP #0 , /"+line.op1);
+                this.file.write("\n");
                 this.file.write(space+"BZ /"+line.res);
                 break;
             case "LOAD_DIRECTION":
@@ -64,42 +62,61 @@ public class FinalCode {
                 break;
             case "LESS":
                 this.file.write(space+"CMP /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"BN $5");
+                this.file.write("\n");
                 this.file.write(space+"MOVE #0 , /"+line.res);
+                this.file.write("\n");
                 this.file.write(space+"BR $3");
+                this.file.write("\n");
                 this.file.write(space+"MOVE #1 , /"+line.res);
                 break;
             case "MORE":
                 this.file.write(space+"CMP /"+line.op2+" , /"+line.op1);
+                this.file.write("\n");
                 this.file.write(space+"BN $5");
+                this.file.write("\n");
                 this.file.write(space+"MOVE #0 , /"+line.res);
+                this.file.write("\n");
                 this.file.write(space+"BR $3");
+                this.file.write("\n");
                 this.file.write(space+"MOVE #1 , /"+line.res);
                 break;
             case "NOTEQUAL":
                 this.file.write(space+"CMP /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"BZ $5");
-                this.file.write(space+"MOVE #1 , /"+line.res);            
+                this.file.write("\n");
+                this.file.write(space+"MOVE #1 , /"+line.res);    
+                this.file.write("\n");
                 this.file.write(space+"BR $3");
+                this.file.write("\n");
                 this.file.write(space+"MOVE #0 , /"+line.res);
                 break;
             case "EQUAL":
                 this.file.write(space+"CMP /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"BZ $5");
+                this.file.write("\n");
                 this.file.write(space+"MOVE #0 , /"+line.res);
+                this.file.write("\n");
                 this.file.write(space+"BR $3");
+                this.file.write("\n");
                 this.file.write(space+"MOVE #1 , /"+line.res);
                 break;
             case "OR":
                 this.file.write(space+"OR /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"MOVE .A , /"+line.res);
                 break;
             case "AND":
                 this.file.write(space+"AND /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"MOVE .A , /"+line.res);
                 break;
             case "PLUS":
                 this.file.write(space+"ADD /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"MOVE .A , /"+line.res);
                 break;
             case "LOAD_NUMBER":
@@ -107,17 +124,21 @@ public class FinalCode {
                 break;
             case "DIVIDE":
                 this.file.write(space+"DIV /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"MOVE .A , /"+line.res);
                 break;
             case "TIMES":
                 this.file.write(space+"MUL /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"MOVE .A , /"+line.res);
                 break;
             case "MINUS":
                 this.file.write(space+"SUB /"+line.op1+" , /"+line.op2);
+                this.file.write("\n");
                 this.file.write(space+"MOVE .A , /"+line.res);
                 break;
         }
+        this.file.write("\n");
     }
     
     public void translateCode(IntermediateCode intermediateFile){
